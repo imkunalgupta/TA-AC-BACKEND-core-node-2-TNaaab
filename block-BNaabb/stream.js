4c2aa3fbf81838287f06dd742428cd6db056282b
Kunal <mailto.guptakunal@gmail.com>
2022-09-22 14:47:37 +0530

    finish

---
+++ block-BNaabb/stream.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+  var store = '';
+  if (req.method === 'POST' && req.url === '/') {
+    req.on('data', (chunk) => {
+      store = store + chunk;
+    });
+    req.on('end', () => {
+      console.log(store);
+    });
+    res.write(store);
+    res.end();
+  }
+}
+server.listen(3456, () => {
+  console.log('Server listening on port 3456');
+});
