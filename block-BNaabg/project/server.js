861ecf15d7431b2dd504e8fef32e2ccddd96e5f8
Kunal <mailto.guptakunal@gmail.com>
2022-09-25 00:36:14 +0530

    core node 2 assignment 3

--- block-BNaabg/project/server.js
+++ block-BNaabg/project/server.js
@@ -4,7 +4,7 @@ var url = require('url');
 const usersPath = __dirname + '/users/';
 var server = http.createServer(handleRequest);
 function handleRequest(req, res) {
-  var parsedUrl = url.parse(req.parse, true);
+  var parsedUrl = url.parse(req.url, true);
   var store = '';
   req.on('data', (chunk) => {
     store += chunk;
@@ -17,11 +17,43 @@ function handleRequest(req, res) {
         fs.writeFile(fd, store, (err) => {
           if (err) return console.log(err);
           fs.close(fd, () => {
-            return res.end(`${username} created successfuly`);
+            return res.end(`${username} created successfully`);
           });
         });
       });
     }
+    if (parsedUrl.pathname === '/users' && req.method === 'GET') {
+      var username = parsedUrl.query.username;
+      fs.readFile(usersPath + username + '.json', (err, content) => {
+        if (err) return console.log(err);
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(content);
+      });
+    }
+    if (parsedUrl.pathname === '/users' && req.method === 'PUT') {
+      var username = parsedUrl.query.username;
+      fs.open(usersPath + username + '.json', 'r+', (err, fd) => {
+        if (err) return console.log(err);
+        fs.ftruncate(fd, (err) => {
+          if (err) return console.log(err);
+          fs.writeFile(fd, store, (err) => {
+            if (err) return console.log(err);
+            fs.close(fd, () => {
+              return res.end(`${username} updated successfully`);
+            });
+          });
+        });
+      });
+    }
+    if (parsedUrl.pathname === '/users' && req.method === 'DELETE') {
+      var username = parsedUrl.query.username;
+      fs.unlink(usersPath + username + '.json', (err) => {
+        if (err) return console.log(err);
+        return res.end(`${username} is Deleted`);
+      });
+    }
+    res.statusCode = 404;
+    res.end('Page not Found');
   });
 }
 server.listen(3000, () => {
